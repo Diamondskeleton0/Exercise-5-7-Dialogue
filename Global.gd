242af91bc0f6f85eecd84544b9c7772fb178a36e
Diamondskeleton0 <iwilliams21212122@gmail.com>
2023-12-03 00:28:17 -0500

    5.7

--- Global.gd
+++ Global.gd
@@ -3,3 +3,11 @@ extends Node
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("menu"):
 		get_tree().quit()
+		
+func shoot():
+	var player = get_node_or_null("/root/Game/Player")
+	if player != null:
+		player.shoot()
+	var zombie = get_node_or_null("/root/Game/Zombie")
+	if zombie != null:
+		zombie.die()
